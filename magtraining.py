a240110372ef5ef5304358e26ffa2ebdc45950e7
WijiMAG <rafael.cartera@myamazonguy.com>
2024-05-30 00:16:53 +0800

    Update magtraining.py

--- magtraining.py
+++ magtraining.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def training():
     #construct your message here
-
-    return message
+    header = "<h1>Training 22</h1>"
+    paragraph = "<p>Hi, my name is Luigi.</p>"
+    message = header + paragraph
+    return render_template_string(message)
